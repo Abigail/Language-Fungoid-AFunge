918ebb24bed50de1e0d025c8043ca414637afee3
Abigail <abigail@abigail.freedom.nl>
2023-10-03 23:19:04 +0200

    First setup for a step() method.

--- lib/Language/Fungoid/AFunge/Interpreter.pm
+++ lib/Language/Fungoid/AFunge/Interpreter.pm
@@ -33,6 +33,44 @@ method set_delta ($dx, $dy) {
     $self;
 }
 
+################################################################################
+#
+# method step ()
+#
+# Advances the position of the interpreter one step. Scans back (or forward)
+# if dropping outside of the program boundaries.
+#
+################################################################################
+
+method step () {
+    $X += $dX;
+    $Y += $dY;
+    my ($min_X, $min_Y, $max_X, $max_Y) = $program -> dimensions;
+    if ($X < $min_X || $X >= $max_X || $Y < $min_Y || $Y >= $max_Y) {
+        #
+        # Out of bounds
+        #
+        my ($min_X, $min_Y, $max_X, $max_Y) = $program -> dimensions;
+
+        my $steps_X = $dX <  0 ? int (($X - $max_X) / $dX)
+                    : $dX == 0 ? 0
+                    :            int (($X - $min_X) / $dX);
+
+        my $steps_Y = $dY <  0 ? int (($Y - $max_Y) / $dY)
+                    : $dY == 0 ? 0
+                    :            int (($Y - $min_Y) / $dY);
+
+        my $steps = $steps_X && $steps_Y ? min ($steps_X,   $steps_Y)
+                                         :      $steps_X || $steps_Y;
+
+        $X -= $steps * $dX;
+        $Y -= $steps * $dY;
+    }
+
+    $self;
+}
+          
+
 
 __END__
 
